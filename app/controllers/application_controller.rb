30e64da111044064c36acf87d3a2bdf73ade597d
dav <dav2070@web.de>
2017-09-23 20:09:25 +0000

    Finish create_object method and use mysql2 as database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,25 @@
 class ApplicationController < ActionController::API
   
-   def check_authorization(dev, api_key, signature)
-      if api_key == dev.api_key
-         sig = dev.id.to_s + "," + dev.user_id.to_s + "," + dev.uuid.to_s
-         
-         digest = OpenSSL::Digest.new('sha256')
-         new_sig = Base64.strict_encode64(OpenSSL::HMAC.hexdigest(digest, dev.secret_key, sig))
-         
-         if new_sig == signature
-            true
+   def check_authorization(api_key, signature)
+      dev = Dev.find_by(api_key: api_key)
+      
+      if !dev
+         false
+      else
+         if api_key == dev.api_key
+            sig = dev.id.to_s + "," + dev.user_id.to_s + "," + dev.uuid.to_s
+            
+            digest = OpenSSL::Digest.new('sha256')
+            new_sig = Base64.strict_encode64(OpenSSL::HMAC.hexdigest(digest, dev.secret_key, sig))
+            
+            if new_sig == signature
+               true
+            else
+               false
+            end
          else
             false
          end
-      else
-         false
       end
    end
 end
--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,50 +1,86 @@
 class AppsController < ApplicationController
    
    # TableObject methods
+   # finished
    def create_object
-      app_id = params["app_id"]     # TODO get this from the api key
       table_name = params["table_name"]
+      app_id = params["app_id"]
+      
+      auth = request.headers['HTTP_AUTHORIZATION'].to_s.length < 2 ? params["auth"].to_s : request.headers['HTTP_AUTHORIZATION'].to_s
+      if auth
+         api_key = auth.split(",")[0]
+         sig = auth.split(",")[1]
+      end
       
       errors = Array.new
       @result = Hash.new
       ok = false
       
-      if !app_id || !table_name || table_name.length < 2
-         errors.push(Array.new([1100, "app_id or table_name is null"]))
+      if !table_name || table_name.length < 2
+         errors.push(Array.new([0000, "Missing field: table_name"]))
          status = 400
-      else
-         # Check if content type is application/json, otherwise send status code 415
+      end
+      
+      if !auth || auth.length < 2
+         errors.push(Array.new([0000, "Missing field: auth"]))
+         status = 401
+      end
+      
+      if !app_id
+         errors.push(Array.new([0000, "Missing field: app_id"]))
+         status = 400
+      end
+      
+      if errors.length == 0
          if request.headers["Content-Type"] != "application/json"
-            errors.push(Array.new([1100, "JSON expected"]))
+            errors.push(Array.new([0000, "Content-type not supported"]))
             status = 415
          else
-            app = App.find_by_id(app_id)
-            if !app
-               errors.push(Array.new([1100, "The app does not exist"]))
+            dev = Dev.find_by(api_key: api_key)
+            
+            if !dev     # Check if the dev exists
+               errors.push(Array.new([0000, "Resource does not exist: Dev"]))
+               status = 400
             else
-               # If the app already has this table, then create it
-               table = Table.find_by(app_id: app_id, name: table_name)
-               if !table
-                  # Create new table
-                  table = Table.new(app_id: app_id, name: table_name)
-                  table.save
+               if !check_authorization(api_key, sig)
+                  errors.push(Array.new([0000, "Authentication failed"]))
+                  status = 401
+               else
+                  app = App.find_by_id(app_id)
+                  # Check if the app exists
+                  if !app
+                     errors.push(Array.new([0000, "Resource does not exist: App"]))
+                     status = 400
+                  else
+                     if app.dev_id != dev.id       # Check if the app belongs to the dev
+                        errors.push(Array.new([0000, "Action not allowed"]))
+                        status = 403
+                     else
+                        table = Table.find_by(name: table_name)
+                        
+                        if !table
+                           # Create a new table
+                           table = Table.new(app_id: app.id, name: table_name)
+                           table.save
+                        end
+                        
+                        obj = TableObject.create(table_id: table.id, name: table_name)
+                        
+                        # Get the body of the request
+                        object = request.request_parameters
+                        
+                        result = Hash.new
+                        result["id"] = obj.id
+                        
+                        object.each do |key, value|
+                           property = Property.create(table_object_id: obj.id, name: key, value: value)
+                           result[key] = value
+                        end
+                        
+                        ok = true
+                     end
+                  end
                end
-               
-               # Create new object
-               obj = TableObject.create(table_id: table.id, name: table_name)
-               
-               # Get the body of the request
-               object = request.request_parameters
-               
-               result = Hash.new
-               result["id"] = obj.id
-               
-               object.each do |key, value|
-                  property = Property.create(table_object_id: obj.id, name: key, value: value)
-                  result[key] = value
-               end
-               
-               ok = true
             end
          end
       end
@@ -58,7 +94,7 @@ class AppsController < ApplicationController
       
       render json: @result, status: status if status
    end
-   
+   # Finished
    def get_object
       object_id = params["object_id"]
       
@@ -90,7 +126,7 @@ class AppsController < ApplicationController
             errors.push(Array.new([0000, "Resource does not exist: Dev"]))
             status = 400
          else
-            if !check_authorization(dev, api_key, sig)
+            if !check_authorization(api_key, sig)
                errors.push(Array.new([0000, "Authentication failed"]))
                status = 401
             else
@@ -195,7 +231,7 @@ class AppsController < ApplicationController
             errors.push(Array.new([0000, "Resource does not exist: Dev"]))
             status = 400
          else
-            if !check_authorization(dev, api_key, sig)
+            if !check_authorization(api_key, sig)
                errors.push(Array.new([0000, "Authentication failed"]))
                status = 401
             else
