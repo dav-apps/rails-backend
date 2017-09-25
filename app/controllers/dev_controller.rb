234016ccd2ba08765f08c5eaacd99fb43acc4b6f
dav <dav2070@web.de>
2017-09-25 20:39:04 +0000

    Add dev controller

---
+++ app/controllers/dev_controller.rb
@@ -0,0 +1,18 @@
+class DevController < ApplicationController
+   
+   define_method :create_dev do
+      
+   end
+   
+   define_method :get_dev do
+      
+   end
+   
+   define_method :update_dev do
+      
+   end
+   
+   define_method :delete_dev do
+      
+   end
+end
