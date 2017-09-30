31af43e7d93246da2ef9d68856190dea6b54e6b1
dav <dav2070@web.de>
2017-09-30 16:11:44 +0000

    Add send_verification_email and send_password_reset_email methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -216,19 +216,26 @@ class UsersController < ApplicationController
                errors.push(Array.new([0000, "Resource does not exist: User"]))
                status = 400
             else
-               requested_user = User.find_by_id(requested_user_id)
+               dev = Dev.find_by_id(dev_id)
                
-               if !requested_user
+               if !dev
                   errors.push(Array.new([0000, "Resource does not exist: User"]))
-                  status = 404
+                  status = 400
                else
-                  # Check if the logged in user is the requested user
-                  if requested_user.id != user.id
-                     errors.push(Array.new([0000, "Action not allowed"]))
-                     status = 403
+                  requested_user = User.find_by_id(requested_user_id)
+                  
+                  if !requested_user
+                     errors.push(Array.new([0000, "Resource does not exist: User"]))
+                     status = 404
                   else
-                     @result = requested_user
-                     ok = true
+                     # Check if the logged in user is the requested user
+                     if requested_user.id != user.id
+                        errors.push(Array.new([0000, "Action not allowed"]))
+                        status = 403
+                     else
+                        @result = requested_user
+                        ok = true
+                     end
                   end
                end
             end
@@ -481,408 +488,97 @@ class UsersController < ApplicationController
       
       render json: @result, status: status if status
    end
-   
-   
-   
-   
-   
-    define_method :set_username do
-        new_username = params["new_username"]
-        jwt = request.headers['HTTP_AUTHORIZATION']
-        
-        errors = Array.new
-        @result = Hash.new
-        ok = false
-        
-        if !new_username || !jwt || new_username.length < 1 || jwt.length < 2
-            errors.push(Array.new([1, "New username or JWT is null"]))
-        else
-            jwt_valid = false
-            begin
-                decoded_jwt = JWT.decode jwt, ENV['JWT_SECRET'], true, { :algorithm => ENV['JWT_ALGORITHM'] }
-                jwt_valid = true
-            rescue JWT::ExpiredSignature
-                # JWT expired
-                errors.push(Array.new([2, "The JWT is expired"]))
-            rescue JWT::DecodeError
-                errors.push(Array.new([3, "The JWT is not valid"]))
-                # rescue other errors
-            rescue Exception
-                errors.push(Array.new([4, "There was an error with your JWT"]))
-            end
-            
-            if new_username.length <= minUsernameLength
-                errors.push(Array.new([5, "Username is too short"]))
-            end
-            
-            if new_username.length > maxUsernameLength
-                errors.push(Array.new([6, "Username is too long"]))
-            end
-            
-            if jwt_valid
-                @user = User.find_by_id(decoded_jwt[0]["id"])
-                
-                if !@user
-                    errors.push(Array.new([7, "This user does not exist"]))
-                else
-                    @user.username = new_username
-                    
-                    if @user.save && errors.length == 0
-                        ok = true
-                    else
-                        @user.errors.each do |e|
-                            if @user.errors[e].any?
-                                @user.errors[e].each do |errorMessage|
-                                    errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        
-        if ok
-            @result["saved"] = true
-        else
-            @result["saved"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
-    
-    def change_email
-        new_email = params["new_email"]
-        jwt = request.headers['HTTP_AUTHORIZATION']
-        ok = false
-        
-        errors = Array.new
-        @result = Hash.new
-        
-        if !new_email || !jwt || new_email.length < 2
-            errors.push(Array.new([1, "New email or JWT is null"]))
-        else
-            jwt_valid = false
-            begin
-                decoded_jwt = JWT.decode jwt, ENV['JWT_SECRET'], true, { :algorithm => ENV['JWT_ALGORITHM'] }
-                jwt_valid = true
-            rescue JWT::ExpiredSignature
-                # JWT expired
-                errors.push(Array.new([2, "The JWT is expired"]))
-            rescue JWT::DecodeError
-                errors.push(Array.new([3, "The JWT is not valid"]))
-                # rescue other errors
-            rescue Exception
-                errors.push(Array.new([4, "There was an error with your JWT"]))
-            end
-            
-            if !validate_email(new_email)
-                errors.push(Array.new([5, "The email is not valid"]))
-            end
-            
-            if jwt_valid
-                @user = User.find_by_id(decoded_jwt[0]["id"])
-                
-                if !@user
-                    errors.push(Array.new([6, "This user does not exist"]))
-                else
-                    @user.old_email = @user.email
-                    @user.new_email = new_email
-                    
-                    if @user.save && errors.length == 0
-                        ok = true
-                    else
-                        @user.errors.each do |e|
-                            if @user.errors[e].any?
-                                @user.errors[e].each do |errorMessage|
-                                    errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        
-        if ok
-            @result["saved"] = true
-            
-            @user.email_confirmation_token = generate_token
-            @user.save
-            
-            # Send email
-            UserNotifier.send_change_email_email(@user).deliver_later
-        else
-            @result["saved"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
-    
-    define_method :change_password do
-        # This method is to change the password from within the account in the settings, JWT required
-        new_password = params["new_password"]
-        jwt = request.headers['HTTP_AUTHORIZATION']
-        ok = false
-        
-        errors = Array.new
-        @result = Hash.new
-        
-        if !jwt || !new_password || jwt.length < 2 || new_password.length < 2
-            errors.push(Array.new([1, "JWT or new_password is null"]))
-        else
-            jwt_valid = false
-            begin
-                decoded_jwt = JWT.decode jwt, ENV['JWT_SECRET'], true, { :algorithm => ENV['JWT_ALGORITHM'] }
-                jwt_valid = true
-            rescue JWT::ExpiredSignature
-                # JWT expired
-                errors.push(Array.new([2, "The JWT is expired"]))
-            rescue JWT::DecodeError
-                errors.push(Array.new([3, "The JWT is not valid"]))
-            rescue Exception
-                # rescue other errors
-                errors.push(Array.new([4, "There was an error with your JWT"]))
-            end
-            
-            @user = User.find_by_id(decoded_jwt[0]["id"])
-            if !@user
-                errors.push(Array.new([5, "This user does not exist"]))
+   # Finished
+   def send_verification_email
+      email = params["email"]
+      
+      errors = Array.new
+      @result = Hash.new
+      ok = false
+      
+      if !email || email.length < 1
+         errors.push(Array.new([0000, "Missing field: email"]))
+         status = 400
+      end
+      
+      if errors.length == 0
+         user = User.find_by(email: email)
+         
+         if !user
+            errors.push(Array.new([0000, "Resource does not exist: User"]))
+            status = 400
+         else
+            if user.confirmed == true
+               errors.push(Array.new([0000, "User is already confirmed"]))
+               status = 400
             else
-                if new_password.length <= minPasswordLength
-                    errors.push(Array.new([6, "The password is too short"]))
-                end
-                puts new_password.length <= minPasswordLength
-                @user.new_password = new_password
-                @user.password_confirmation_token = generate_token
-                
-                if @user.save && errors.length == 0
-                    ok = true
-                else
-                    @user.errors.each do |e|
-                        if @user.errors[e].any?
-                            @user.errors[e].each do |errorMessage|
-                                errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                            end
-                        end
-                    end
-                end
+               user.email_confirmation_token = generate_token
+               if !user.save
+                  errors.push(Array.new([0000, "Unknown validation error"]))
+                  status = 500
+               else
+                  ok = true
+               end
             end
-        end
-        
-        if ok
-            @result["saved"] = true
-            
-            # Send email
-            UserNotifier.send_change_password_email(@user).deliver_later
-        else
-            @result["saved"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
-    
-    def set_avatar_file_extension
-        ext = params["ext"]
-        jwt = request.headers['HTTP_AUTHORIZATION']
-        ok = false
-        
-        errors = Array.new
-        @result = Hash.new
-        
-        if !jwt || !ext || jwt.length < 2 || ext.length < 3
-            errors.push(Array.new([1, "JWT or ext is null"]))
-        else
-            jwt_valid = false
-            begin
-                decoded_jwt = JWT.decode jwt, ENV['JWT_SECRET'], true, { :algorithm => ENV['JWT_ALGORITHM'] }
-                jwt_valid = true
-            rescue JWT::ExpiredSignature
-                # JWT expired
-                errors.push(Array.new([2, "The JWT is expired"]))
-            rescue JWT::DecodeError
-                errors.push(Array.new([3, "The JWT is not valid"]))
-            rescue Exception
-                # rescue other errors
-                errors.push(Array.new([4, "There was an error with your JWT"]))
-            end
-            
-            if jwt_valid
-                @user = User.find_by_id(decoded_jwt[0]["id"])
-                
-                if !@user
-                    errors.push(Array.new([5, "This user does not exist"]))
-                else
-                    @user.avatar_file_extension = ext
-                    
-                    if @user.save && errors.length == 0
-                        ok = true
-                    else
-                        @user.errors.each do |e|
-                            if @user.errors[e].any?
-                                @user.errors[e].each do |errorMessage|
-                                    errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        
-        if ok
-            @result["saved"] = true
-        else
-            @result["saved"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
-    
-    # Dev routes
-    def confirm_user
-        id = params[:id]
-        confirmation_token = params[:confirmation_token]
-        
-        errors = Array.new
-        @result = Hash.new
-        
-        confirmed = false
-        
-        if !id || !confirmation_token || id.length < 1
-            errors.push(Array.new([1, "ID or confirmation_token is null"]))
-        else
-            @user = User.find_by_id(id)
-            
-            if !@user
-                errors.push(Array.new([2, "This user does not exist"]))
+         end
+      end
+      
+      if ok && errors.length == 0
+         status = 200
+         # Send email
+         UserNotifier.send_signup_email(user).deliver_later
+      else
+         @result.clear
+         @result["errors"] = errors
+      end
+      
+      render json: @result, status: status if status
+   end
+   # finished
+   def send_password_reset_email
+      email = params["email"]
+      
+      errors = Array.new
+      @result = Hash.new
+      ok = false
+      
+      if !email || email.length < 1
+         errors.push(Array.new([0000, "Missing field: email"]))
+         status = 400
+      end
+      
+      if errors.length == 0
+         user = User.find_by(email: email)
+         
+         if !user
+            errors.push(Array.new([0000, "Resource does not exist: User"]))
+            status = 400
+         else
+            # Generate password confirmation token
+            user.password_confirmation_token = generate_token
+            if !user.save
+               errors.push(Array.new([0000, "Unknown validation error"]))
+               status = 500
             else
-                if @user.confirmed
-                    errors.push(Array.new([3, "Your account is already confirmed"]))
-                elsif @user.email_confirmation_token == confirmation_token
-                    # Confirm user
-                    @user.confirmed = true
-                    if @user.save && errors.length == 0
-                        confirmed = true
-                        @user.email_confirmation_token = nil
-                        @user.save
-                    else
-                        @user.errors.each do |e|
-                            if @user.errors[e].any?
-                                @user.errors[e].each do |errorMessage|
-                                    errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                                end
-                            end
-                        end
-                    end
-                else
-                    errors.push(Array.new([4, "The confirmation token is not correct"]))
-                end
+               ok = true
             end
-        end
-        
-        if confirmed
-            @result["confirmed"] = true
-        else
-            @result["confirmed"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
+         end
+      end
+      
+      if ok && errors.length == 0
+         status = 200
+         # Send email
+         UserNotifier.send_password_reset_email(user).deliver_later
+      else
+         @result.clear
+         @result["errors"] = errors
+      end
+      
+      render json: @result, status: status if status
+   end
     
-    def send_verification_email
-        email = params[:email]
-        @user = User.find_by_email(email)
-        ok = false
-        
-        errors = Array.new
-        @result = Hash.new
-        
-        if !email || email.length < 2
-            errors.push(Array.new([1, "Email is null"]))
-        else
-            if !@user
-                errors.push(Array.new([2, "A user with that email does not exist"]))
-            else
-                if @user.confirmed
-                    errors.push(Array.new([3, "Your account is already confirmed"]))
-                else
-                    # Generate email_confirmation_token and save in DB
-                    @user.email_confirmation_token = generate_token
-                    if @user.save && errors.length == 0
-                        ok = true
-                    else
-                        @user.errors.each do |e|
-                            if @user.errors[e].any?
-                                @user.errors[e].each do |errorMessage|
-                                    errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        
-        if ok
-            @result["sent"] = true
-            UserNotifier.send_signup_email(@user).deliver_later
-        else
-            @result["sent"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
     
-    def send_password_reset_email
-        email = params["email"]
-        ok = false
-        
-        errors = Array.new
-        @result = Hash.new
-        
-        if !email || email.length < 2
-            errors.push(Array.new([1, "Email is null"]))
-        else
-            @user = User.find_by_email(email)
-            
-            if !@user
-                errors.push(Array.new([2, "A user with that email does not exist"]))
-            else
-                # Generate password_confirmation_token and safe in DB
-                @user.password_confirmation_token = generate_token
-                if @user.save && errors.length == 0
-                    ok = true
-                else
-                    @user.errors.each do |e|
-                        if @user.errors[e].any?
-                            @user.errors[e].each do |errorMessage|
-                                errors.push(Array.new([0, e.to_s + " " + errorMessage.to_s]))
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        
-        if ok
-            @result["sent"] = true
-            
-            # Send email
-            UserNotifier.send_password_reset_email(@user).deliver_later
-        else
-            @result["sent"] = false
-            @result["errors"] = errors
-        end
-        
-        @result = @result.to_json.html_safe
-    end
+    
     
     def check_password_confirmation_token
         id = params["id"]
@@ -1121,13 +817,13 @@ class UsersController < ApplicationController
     end
     
     
-    private
-    def generate_token
-        SecureRandom.hex(20)
-    end
+   private
+   def generate_token
+      SecureRandom.hex(20)
+   end
     
-    def validate_email(email)
-        reg = Regexp.new("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-        return (reg.match(email))? true : false
-    end
+   def validate_email(email)
+      reg = Regexp.new("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+      return (reg.match(email))? true : false
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
   
-  # Authentication routes
-  # get 'user/login', to: 'users#login'
-  # get 'user/signup', to: 'users#signup'
-  get 'user/set_username', to: 'users#set_username'
-  get 'user/confirm_user', to: 'users#confirm_user'
-  get 'user/send_verification_email', to: 'users#send_verification_email'
-  get 'user/send_password_reset_email', to: 'users#send_password_reset_email'
-  get 'user/check_password_confirmation_token', to: 'users#check_password_confirmation_token'
-  get 'user/save_new_password', to: 'users#save_new_password'
-  get 'user/change_email', to: 'users#change_email'
-  get 'user/confirm_new_email', to: 'users#confirm_new_email'
-  get 'user/reset_new_email', to: 'users#reset_new_email'
-  get 'user/change_password', to: 'users#change_password'
-  get 'user/confirm_new_password', to: 'users#confirm_new_password'
-  get 'user/set_avatar_file_extension', to: 'users#set_avatar_file_extension'
-  
   # Cards routes
   get 'cards/get_all_decks_and_cards', to: 'cards#get_all_decks_and_cards'
   get 'cards/create_deck', to: 'cards#create_deck'
@@ -25,16 +9,15 @@ Rails.application.routes.draw do
   get 'cards/update_card', to: 'cards#update_card'
   get 'cards/delete_card', to: 'cards#delete_card'
   
-  # Analytics routes
-  # post '/v1/analytics/event', to: 'analytics#create'
-  match '/v1/analytics/event', to: 'analytics#create', via: 'post'
-  
   # User routes
   match '/v1/users/login', to: 'users#login', via: 'get'
   match '/v1/users/signup', to: 'users#signup', via: 'post'
   match '/v1/users/:id', to: 'users#get_user', via: 'get'
   match '/v1/users/:id', to: 'users#update_user', via: 'put'
   match '/v1/users/:id', to: 'users#delete_user', via: 'delete'
+  
+  match '/v1/users/send_password_reset_email', to: 'users#send_password_reset_email', via: 'post'
+  match '/v1/users/send_verification_email', to: 'users#send_verification_email', via: 'post'
   
   # Apps routes
   match '/v1/apps/object', to: 'apps#create_object', via: 'post'
@@ -51,4 +34,7 @@ Rails.application.routes.draw do
   match '/v1/apps/app', to: 'apps#get_app', via: 'get'
   match '/v1/apps/app', to: 'apps#update_app', via: 'put'
   match '/v1/apps/app', to: 'apps#delete_app', via: 'delete'
+  
+  # Analytics routes
+  match '/v1/analytics/event', to: 'analytics#create', via: 'post'
 end
