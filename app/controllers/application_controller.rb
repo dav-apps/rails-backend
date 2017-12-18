15ee2286e2c99a2de15f72a9323244a2d0c407ce
dav <dav2070@web.de>
2017-12-18 16:38:50 +0000

    Add Access-Control headers to response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+   
+   after_action :set_cors_header
   
    def check_authorization(api_key, signature)
       dev = Dev.find_by(api_key: api_key)
@@ -8,7 +10,6 @@ class ApplicationController < ActionController::API
       else
          if api_key == dev.api_key
             
-            #new_sig = Base64.strict_encode64(Base64.strict_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), dev.secret_key, dev.uuid)))
             new_sig = Base64.strict_encode64(OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), dev.secret_key, dev.uuid))
             
             if new_sig == signature
