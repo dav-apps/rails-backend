7459e1c1d7f6122ba37af1a60d6fbc4e46cc77e8
dav <dav2070@web.de>
2017-12-20 22:27:18 +0000

    Allow more headers for Cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Workspace
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => ['HTTP_AUTHORIZATION', 'Authentication', 'Content-Type'], :methods => [:get, :post, :put, :delete, :options]
+        resource '*', :headers => ['HTTP_AUTHORIZATION', 'Authentication', 'Content-Type'], :methods => [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
   end
