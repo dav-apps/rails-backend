4727c495ca1e597636e13f67d35de29bfd23d834
Dav <dav2070@web.de>
2017-12-22 14:33:56 +0100

    Add new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,51 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-users = User.create([{username: "sherlock", email: "sherlock@example.com", password: "sherlocked"}, 
-                    {username: "matt", email: "matt@test.com", password: "schachmatt"},
-                    {username: "testuser", email: "testuser@email.com", password: "testpassword"}])
-devs = Dev.create([{user: users.first},  # Sherlock
-                    {user: users.second}]) # Matt
+users = User.create([
+   {email: "dav@dav-apps.tech", password: "davdavdav", username: "dav", confirmed: true},
+   {email: "test@example.com", password: "password", username: "testuser", confirmed: true},
+   {email: "nutzer@testemail.com", password: "blablablablabla", username: "nutzer", confirmed: false},
+   {email: "normalo@helloworld.net", password: "schoeneheilewelt", username: "normalo", confirmed: true}
+])
 
-apps = App.create([{name: "TestApp", description: "This is a test app.", dev: devs.second}, # Matt
-                    {name: "Cards", description: "This is an app for learning vocabulary.", dev: devs.first}]) # Sherlock
+devs = Dev.create([
+   {user: users.first, api_key: "eUzs3PQZYweXvumcWvagRHjdUroGe5Mo7kN1inHm", secret_key: "Stac8pRhqH0CSO5o9Rxqjhu7vyVp4PINEMJumqlpvRQai4hScADamQ", uuid: "d133e303-9dbb-47db-9531-008b20e5aae8"},
+   {user: users.second, api_key: "MhKSDyedSw8WXfLk2hkXzmElsiVStD7C8JU3KNGp", secret_key: "5nyf0tRr0GNmP3eB83pobm8hifALZsUq3NpW5En9nFRpssXxlZv-JA", uuid: "71a5d4f8-083e-413e-a8ff-66847a5f3a97"}
+])
 
-tables = Table.create([{name: "Note", app: apps.first}, {name: "Card", app: apps.second}])
+apps = App.create([
+   {dev: devs.first, name: "Cards", description: "This is a vocabulary app!"},
+   {dev: devs.second, name: "TestApp", description: "This is a test app."}
+])
 
-objects = TableObject.create([{table: tables.first, user: users[0]},
-                            {table: tables.first, user: users[0]},
-                            {table: tables.first, user: users[1]},
-                            {table: tables.first, user: users[1]},
-                            {table: tables.first, user: users[2]},
-                            {table: tables.first, user: users[2]},
-                            {table: tables.second, user: users[0]},
-                            {table: tables.second, user: users[0]},
-                            {table: tables.second, user: users[1]},
-                            {table: tables.second, user: users[1]},
-                            {table: tables.second, user: users[2]},
-                            {table: tables.second, user: users[2]}])
-
-properties = Property.create([{table_object: objects[0], name: "title", value: "Hello World"},
-                            {table_object: objects[0], name: "content", value: "This is my first note. Hello World!"},
-                            {table_object: objects[1], name: "title", value: "Welt retten"},
-                            {table_object: objects[1], name: "content", value: "Samstag, 9:30 in Petropavlovsk-Kamshatsky"},
-                            {table_object: objects[2], name: "title", value: "Todos"},
-                            {table_object: objects[2], name: "content", value: "1. Welt retten, 2. Meerschweinchen retten, 3. Hackbrett üben"},
-                            {table_object: objects[3], name: "title", value: "Weltherschaftsplan"},
-                            {table_object: objects[3], name: "content", value: "Den richtigen Namen von L herausfinden und ihn ins Death Note schreiben"},
-                            {table_object: objects[4], name: "title", value: "Echter Name von L"},
-                            {table_object: objects[4], name: "content", value: "L Lawliet"},
-                            {table_object: objects[5], name: "title", value: "ACHTUNG SPOILER!!1!"},
-                            {table_object: objects[5], name: "content", value: "Tschubakka stirbt am Ende von Harry Potter"},
-                            {table_object: objects[6], name: "page1", value: "Hello World"},
-                            {table_object: objects[6], name: "page2", value: "Hallo Welt"},
-                            {table_object: objects[7], name: "page1", value: "Ubiquity"},
-                            {table_object: objects[7], name: "page2", value: "Allgegenwärtigkeit"},
-                            {table_object: objects[8], name: "page1", value: "blitzkrieg"},
-                            {table_object: objects[8], name: "page2", value: "Blitzkrieg"},
-                            {table_object: objects[9], name: "page1", value: "entrails"},
-                            {table_object: objects[9], name: "page2", value: "Eingeweide"},
-                            {table_object: objects[10], name: "page1", value: "mischievously"},
-                            {table_object: objects[10], name: "page2", value: "bösartig"},
-                            {table_object: objects[11], name: "page1", value: "ferocity"},
-                            {table_object: objects[11], name: "page2", value: "Bösartigkeit"}])
+tables = Table.create([
+   {name: "Card", app: apps.first},
+   {name: "TestTable", app: apps.second}
+])
