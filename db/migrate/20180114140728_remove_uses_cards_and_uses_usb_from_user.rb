4d556d43d4f865c2458c2cfa2dc7a98a2450e773
Dav <dav2070@web.de>
2018-01-14 15:28:54 +0100

    Remove the newest migration

--- db/migrate/20180114140728_remove_uses_cards_and_uses_usb_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUsesCardsAndUsesUsbFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :uses_cards, :boolean
-    remove_column :users, :uses_usb, :boolean
-  end
-end
