e1eb0619d587d1ac39c670c73e1e6d3a910abf89
Dav <dav2070@web.de>
2018-05-13 23:51:51 +0200

    Configure CORS rules

--- config/application.rb
+++ config/application.rb
@@ -7,33 +7,43 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Workspace
-  class Application < Rails::Application
-    config.api_only = true
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+  	class Application < Rails::Application
+    	config.api_only = true
+    	# Settings in config/environments/* take precedence over those specified here.
+    	# Application configuration should go into files in config/initializers
+    	# -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+		# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+		# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+		# config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+		# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+		# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+		# config.i18n.default_locale = :de
 
-    # Use sidekiq for async jobs
-    config.active_job.queue_adapter = :sidekiq
+		# Use sidekiq for async jobs
+		config.active_job.queue_adapter = :sidekiq
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+		# Do not swallow errors in after_commit/after_rollback callbacks.
+		config.active_record.raise_in_transactional_callbacks = true
 
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-          headers: :any,
-          methods: %i(get post put patch delete options head)
-      end
-    end
-  end
+		Rails.application.config.middleware.insert_before 0, Rack::Cors do
+			allow do
+				origins ENV['BASE_URL'], 'dav-apps-staging.herokuapp.com'
+
+				resource '*',
+				headers: :any,
+          	methods: %i(get post put patch delete options head)
+			end
+
+      	allow do
+				origins '*'
+
+				resource '*', headers: :any, methods: :any
+
+				# Only allow get on auth routes
+				resource '/v1/auth/**/*', methods: [:get], headers: :any
+      	end
+    	end
+  	end
 end
