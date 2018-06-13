1f8c589672a5a0739f01b08b69d9a209dca34d0c
Dav <dav2070@web.de>
2018-06-13 22:22:36 +0200

    Remove the not null statement in the timestamps migration

--- db/migrate/20180613180038_add_timestamps_to_users_apps.rb
+++ db/migrate/20180613180038_add_timestamps_to_users_apps.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToUsersApps < ActiveRecord::Migration
   def change
-    add_column :users_apps, :created_at, :datetime, null: false
-    add_column :users_apps, :updated_at, :datetime, null: false
+    add_column :users_apps, :created_at, :datetime
+    add_column :users_apps, :updated_at, :datetime
   end
 end
