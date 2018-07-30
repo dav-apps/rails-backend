2fa0e0a178252fb7e435b8f05794bf2783e9338e
David <dav2070@web.de>
2018-07-31 01:43:21 +0200

    Add settings for CORS

--- config/application.rb
+++ config/application.rb
@@ -17,17 +17,33 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module Workspace
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+  	class Application < Rails::Application
+    	# Initialize configuration defaults for originally generated Rails version.
+    	config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+		# Settings in config/environments/* take precedence over those specified here.
+		# Application configuration should go into files in config/initializers
+		# -- all .rb files in that directory are automatically loaded.
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-  end
+		# Only loads a smaller set of middleware suitable for API only apps.
+		# Middleware like session, flash, cookies can be added back manually.
+		# Skip views, helpers and assets when generating a new resource.
+		config.api_only = true
+
+    	Rails.application.config.middleware.insert_before 0, Rack::Cors do
+			allow do
+				origins 	ENV['BASE_URL'],
+							'localhost:3001',
+							'cards-dav.azurewebsites.net',
+							'calendo-dav.azurewebsites.net',
+							'calendo.dav-apps.tech',
+							'blog.dav-apps.tech'
+				
+
+				resource '*',
+				headers: :any,
+          	methods: %i(get post put patch delete options head)
+			end
+    	end
+  	end
 end
