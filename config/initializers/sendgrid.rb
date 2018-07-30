ce088fa099516a036f710128e3ed43108e2fcc95
David <dav2070@web.de>
2018-07-30 19:34:14 +0200

    Remove old files, add sendgrid initialization code and force ssl

---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,10 @@
+# Sendgrid configuration
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'dav-apps.tech',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
