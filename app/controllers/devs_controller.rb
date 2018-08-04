7f8ba39d2bcfd49e385f171329bcdbf0217b26d3
David <dav2070@web.de>
2018-08-04 13:24:32 +0200

    Refactor DevsController.get_dev

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -36,82 +36,42 @@ class DevsController < ApplicationController
       end
    end
 
-   define_method :get_dev do
+   def get_dev
       jwt = request.headers['HTTP_AUTHORIZATION'].to_s.length < 2 ? params["jwt"].to_s.split(' ').last : request.headers['HTTP_AUTHORIZATION'].to_s.split(' ').last
-      
-      errors = Array.new
-      @result = Hash.new
-      ok = false
-      
-      if !jwt || jwt.length < 1
-         errors.push(Array.new([2102, "Missing field: jwt"]))
-         status = 401
-      end
-      
-      if errors.length == 0
-         jwt_valid = false
-         begin
-            decoded_jwt = JWT.decode jwt, ENV['JWT_SECRET'], true, { :algorithm => ENV['JWT_ALGORITHM'] }
-            jwt_valid = true
-         rescue JWT::ExpiredSignature
-            # JWT expired
-            errors.push(Array.new([1301, "JWT: expired"]))
-            status = 401
-         rescue JWT::DecodeError
-            errors.push(Array.new([1302, "JWT: not valid"]))
-            status = 401
-            # rescue other errors
-         rescue Exception
-            errors.push(Array.new([1303, "JWT: unknown error"]))
-            status = 401
-         end
-         
-         if jwt_valid
-            user_id = decoded_jwt[0]["user_id"]
-            dev_id = decoded_jwt[0]["dev_id"]
-            
-            user = User.find_by_id(user_id)
-            
-            if !user
-               errors.push(Array.new([2801, "Resource does not exist: User"]))
-               status = 400
-            else
-               dev = Dev.find_by_id(dev_id)
-               
-               if !dev || !user.dev
-                  errors.push(Array.new([2802, "Resource does not exist: Dev"]))
-                  status = 404
-               else
-                  # Make sure this is called from the website
-                  if dev != Dev.first
-                     errors.push(Array.new([1102, "Action not allowed"]))
-                     status = 403
-                  else
-                     # Return the dev object
-							@result = user.dev.attributes
 
-							apps_array = Array.new
-							user.dev.apps.each { |app| apps_array.push(app) }
+      begin
+         ValidationService.raise_validation_error(ValidationService.validate_jwt_missing(jwt))
 
-							@result["apps"] = apps_array
+         jwt_signature_validation = ValidationService.validate_jwt_signature(jwt)
+			ValidationService.raise_validation_error(jwt_signature_validation[0])
+			user_id = jwt_signature_validation[1][0]["user_id"]
+         dev_id = jwt_signature_validation[1][0]["dev_id"]
 
-                     ok = true
-                  end
-               end
-            end
-         end
+         user = User.find_by_id(user_id)
+			ValidationService.raise_validation_error(ValidationService.validate_user_does_not_exist(user))
+
+			dev = Dev.find_by_id(dev_id)
+         ValidationService.raise_validation_error(ValidationService.validate_dev_does_not_exist(dev))
+
+         ValidationService.raise_validation_error(ValidationService.validate_dev_is_first_dev(dev))
+
+         # Return the dev object
+         result = user.dev.attributes
+
+         apps_array = Array.new
+         user.dev.apps.each { |app| apps_array.push(app) }
+
+         result["apps"] = apps_array
+         render json: result, status: 200
+      rescue RuntimeError => e
+         validations = JSON.parse(e.message)
+			result = Hash.new
+			result["errors"] = ValidationService.get_errors_of_validations(validations)
+
+			render json: result, status: validations.last["status"]
       end
-      
-      if ok && errors.length == 0
-         status = 200
-      else
-         @result.clear
-         @result["errors"] = errors
-      end
-      
-      render json: @result, status: status if status
    end
-   
+
    define_method :get_dev_by_api_key do
       requested_dev_api_key = params["api_key"]
       auth = request.headers['HTTP_AUTHORIZATION'].to_s.length < 2 ? params["auth"].to_s.split(' ').last : request.headers['HTTP_AUTHORIZATION'].to_s.split(' ').last
