b33df48acd0bcc505e65e8d9facf5793e9d24be9
Dav <dav2070@web.de>
2018-08-20 20:57:33 +0200

    Add seed for another table object

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,8 @@ tables = Table.create([
 
 table_objects = TableObject.create([
 	{table: tables[2], user: users[4], uuid: "642e6407-f357-4e03-b9c2-82f754931161", file: false},
-	{table: tables[2], user: users[4], uuid: "8d29f002-9511-407b-8289-5ebdcb5a5559", file: false}
+   {table: tables[2], user: users[4], uuid: "8d29f002-9511-407b-8289-5ebdcb5a5559", file: false},
+   {table: tables[3], user: users[4], uuid: "4c8513e8-67c3-4067-8d80-bc2ed0459918", file: false}
 ])
 
 properties = Property.create([
