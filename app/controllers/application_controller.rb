78591a1a1846997a0dc12c16f9aca95dd8334758
Dav <dav2070@web.de>
2019-03-22 19:32:13 +0100

    Introduce pro plan with 50 GB and change the storage values of the other plans

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,13 +68,16 @@ class ApplicationController < ActionController::API
 
 	def get_total_storage(plan, confirmed)
 		storage_unconfirmed = 1000000000 	# 1 GB
-      storage_on_free_plan = 5000000000 	# 5 GB
-		storage_on_plus_plan = 50000000000 	# 50 GB
+      storage_on_free_plan = 2000000000 	# 2 GB
+      storage_on_plus_plan = 15000000000 	# 15 GB
+      storage_on_pro_plan = 50000000000   # 50 GB
 
 		if !confirmed
 			return storage_unconfirmed
       elsif plan == 1 # User is on Plus plan
 			return storage_on_plus_plan
+		elsif plan == 2
+			return storage_on_pro_plan
 		else
 			return storage_on_free_plan
 		end
