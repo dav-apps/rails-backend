ea1b5789aeb79c523642a75f1b448c8908f02515
David <dav2070@web.de>
2019-11-29 12:58:57 +0100

    Add support for function call as part of other expressions
    - Merge resolve_expression and execute_exp methods to execute_command

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -53,7 +53,7 @@ class ApisController < ApplicationController
 			ast = parser.parse_string(api_endpoint.commands)
 
 			ast.each do |element|
-				execute_exp(element)
+				execute_command(element)
 			end
 
 		rescue RuntimeError => e
@@ -63,30 +63,65 @@ class ApisController < ApplicationController
 	end
 
 	private
-	def execute_exp(exp)
-		if exp.class == Array
-			call_function(exp)
-		else
-			puts exp
-		end
-	end
+	def execute_command(command)
+		if command.class == Array
+			# Command is a function call
+			if command[0] == :var
+				set_var(command[1], execute_command(command[2]))
+			elsif command[0] == :if && command[3] == :else
+				resolve_if(command)
+			elsif command[0] == :log
+				puts execute_command(command[1])
+			elsif command[0] == :to_int
+				value = get_var(command[1])
+				set_var(command[1], value.to_i) if value
+				return value.to_i
+			elsif command[0] == :render_json
+				render json: execute_command(command[1]), status: execute_command(command[2])
+			
+			# Command is an expression
+			elsif command[1] == :==
+				execute_command(command[0]) == execute_command(command[2])
+			elsif command[1] == :!=
+				execute_command(command[0]) != execute_command(command[2])
+			elsif command[1] == :+ || command[1] == :-
+				if execute_command(command[0]).class == Integer
+					result = 0
+				elsif execute_command(command[0]).class == String
+					result = ""
+				end
 
-	def call_function(exp)
-		if exp[0] == :var
-			set_var(exp[1], resolve_expression(exp[2]))
-		elsif exp[0] == :if && exp[3] == :else
-			resolve_if(exp)
-		elsif exp[0] == :log
-			puts resolve_expression(exp[1])
-		elsif exp[0] == :to_int
-			value = get_var(exp[1])
-			set_var(exp[1], value.to_i) if value
-		elsif exp[0] == :render_json
-			render json: resolve_expression(exp[1]), status: resolve_expression(exp[2])
-		else
-			exp.each do |command|
-				execute_exp(command)
+				i = 0
+				while command[i]
+					if command[i - 1] == :- && result.class == Integer
+						result -= execute_command(command[i])
+					else
+						# Add the next part to the result
+						if result.class == String
+							result += execute_command(command[i]).to_s
+						else
+							result += execute_command(command[i])
+						end
+					end
+
+					i += 2
+				end
+				result
+			else
+				command.each do |command|
+					execute_command(command)
+				end
 			end
+		elsif !!command == command
+			# command is boolean
+			command
+		else
+			# Find and return the variable
+			var = get_var(command)
+			return var if var
+
+			# Return the command
+			command
 		end
 	end
 
@@ -102,53 +137,10 @@ class ApisController < ApplicationController
 	end
 
 	def resolve_if(exp)
-		if resolve_expression(exp[1])
-			execute_exp(exp[2])
+		if execute_command(exp[1])
+			execute_command(exp[2])
 		else
-			execute_exp(exp[4])
-		end
-	end
-
-	def resolve_expression(exp)
-		if exp.class == Array
-			if exp[1] == :==
-				resolve_expression(exp[0]) == resolve_expression(exp[2])
-			elsif exp[1] == :!=
-				resolve_expression(exp[0]) != resolve_expression(exp[2])
-			elsif exp[1] == :+ || exp[1] == :-
-				if resolve_expression(exp[0]).class == Integer
-					result = 0
-				elsif resolve_expression(exp[0]).class == String
-					result = ""
-				end
-
-				i = 0
-				while exp[i]
-					if exp[i - 1] == :- && result.class == Integer
-						result -= resolve_expression(exp[i])
-					else
-						# Add the next part to the result
-						if result.class == String
-							result += resolve_expression(exp[i]).to_s
-						else
-							result += resolve_expression(exp[i])
-						end
-					end
-
-					i += 2
-				end
-				result
-			end
-		elsif !!exp == exp
-			# exp is boolean
-			exp
-		else
-			# Find the variable
-			var = get_var(exp)
-			return var if var
-
-			# Return the expression
-			exp
+			execute_command(exp[4])
 		end
 	end
 end
