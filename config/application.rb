3b7007823eac44b55a137ba90ff13356f202a11a
David <dav2070@web.de>
2020-12-11 16:46:23 +0100

    Migrate app to Rails 6

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
@@ -17,19 +20,20 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module Workspace
-  	class Application < Rails::Application
-    	# Initialize configuration defaults for originally generated Rails version.
-    	config.load_defaults 5.1
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
-		# Settings in config/environments/* take precedence over those specified here.
-		# Application configuration should go into files in config/initializers
-		# -- all .rb files in that directory are automatically loaded.
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
-		# Only loads a smaller set of middleware suitable for API only apps.
-		# Middleware like session, flash, cookies can be added back manually.
-		# Skip views, helpers and assets when generating a new resource.
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
 		config.api_only = true
-
+		
 		config.action_cable.allowed_request_origins = [
 			"http://localhost:3002",
 			"http://localhost:3001",
@@ -40,14 +44,14 @@ module Workspace
 			"https://dav-website-staging.azurewebsites.net",
 			"https://calendo.dav-apps.tech",
 			"https://calendo-dav.azurewebsites.net",
-         "https://calendo-dav-staging.azurewebsites.net",
-         "https://pocketlib.dav-apps.tech",
-         "https://pocketlib-dav.azurewebsites.net",
-         "https://pocketlib-dav-staging.azurewebsites.net",
+      "https://calendo-dav-staging.azurewebsites.net",
+			"https://pocketlib.dav-apps.tech",
+    	"https://pocketlib-dav.azurewebsites.net",
+    	"https://pocketlib-dav-staging.azurewebsites.net",
 			nil
 		]
 
-    	Rails.application.config.middleware.insert_before 0, Rack::Cors do
+		Rails.application.config.middleware.insert_before 0, Rack::Cors do
 			allow do
 				origins 	ENV['BASE_URL'],
 							'blog.dav-apps.tech',
@@ -71,5 +75,5 @@ module Workspace
           	methods: %i(get post put patch delete options head)
 			end
 		end
-  	end
+  end
 end
