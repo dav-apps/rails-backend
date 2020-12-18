1e0360bee180fe5c4495d88a02050cd7ec7be203
David <dav2070@web.de>
2020-12-18 23:03:45 +0100

    Remove AccessToken and TableObjectsAccessToken models & fixtures
    - Remove visibility from TableObject fixtures

--- app/models/access_token.rb
+++
@@ -1,4 +0,0 @@
-class AccessToken < ApplicationRecord
-   has_many :table_objects_access_token, dependent: :destroy
-   has_many :table_objects, through: :table_objects_access_token
-end
--- app/models/table_objects_access_token.rb
+++
@@ -1,4 +0,0 @@
-class TableObjectsAccessToken < ApplicationRecord
-   belongs_to :table_object
-   belongs_to :access_token
-end
