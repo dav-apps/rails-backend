488f81d9426c0ffd36a65d7813f048685b328c25
David <dav2070@web.de>
2020-12-25 13:57:40 +0100

    Set and update the etag of table objects in create, update and get table object endpoints

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -512,6 +512,10 @@ class AppsController < ApplicationController
 					properties[name] = value
 				end
 
+				# Generate the etag
+				obj.etag = generate_table_object_etag(obj)
+				ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(obj.save))
+
 				# Save that user uses the app
 				users_app = UsersApp.find_by(app_id: app.id, user_id: user.id)
 				if !users_app
@@ -529,7 +533,7 @@ class AppsController < ApplicationController
 				# Return the data
 				result = obj.attributes
 				result["properties"] = properties
-				result["etag"] = generate_table_object_etag(obj)
+				result["etag"] = obj.etag
 
 				render json: result, status: 201
 			else
@@ -585,6 +589,10 @@ class AppsController < ApplicationController
 						properties[prop.name] = prop.value
 					end
 
+					# Generate the etag
+					obj.etag = generate_table_object_etag(obj)
+					ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(obj.save))
+
 					# Save that the user was active
 					user.update_column(:last_active, Time.now)
 					users_app.update_column(:last_active, Time.now)
@@ -593,7 +601,7 @@ class AppsController < ApplicationController
 					TableObjectUpdateChannel.broadcast_to("#{user.id},#{app.id}", uuid: obj.uuid, change: 0, session_id: session_id)
 
 					result["properties"] = properties
-					result["etag"] = generate_table_object_etag(obj)
+					result["etag"] = obj.etag
 					render json: result, status: 201
 				rescue Exception => e
 					ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(false))
@@ -714,6 +722,12 @@ class AppsController < ApplicationController
 				response.headers['Content-Length'] = result.size.to_s
 				send_data(result, status: 200, type: type, filename: filename)
 			else
+				# Generate the etag if the table object has none
+				if obj.etag.nil?
+					obj.etag = generate_table_object_etag(obj)
+					obj.save
+				end
+
 				# Return the object data
 				result = obj.attributes
 				property_types = table.property_types
@@ -725,7 +739,7 @@ class AppsController < ApplicationController
 				end
 
 				result["properties"] = properties
-				result["etag"] = generate_table_object_etag(obj)
+				result["etag"] = obj.etag
 				result["table_id"] = table_id
 
 				render json: result, status: 200
@@ -798,6 +812,12 @@ class AppsController < ApplicationController
 				response.headers['Content-Length'] = result.size.to_s
 				send_data(result, status: 200, type: type, filename: filename)
 			else
+				# Generate the etag if the table object has none
+				if obj.etag.nil?
+					obj.etag = generate_table_object_etag(obj)
+					obj.save
+				end
+
 				# Return the data
 				result = obj.attributes
 				property_types = table.property_types
@@ -809,7 +829,7 @@ class AppsController < ApplicationController
 				end
 
 				result["properties"] = properties
-				result["etag"] = generate_table_object_etag(obj)
+				result["etag"] = obj.etag
 
 				render json: result, status: 200
 			end
@@ -932,6 +952,10 @@ class AppsController < ApplicationController
 				ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(size_prop.save))
 				ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(etag_prop.save))
 
+				# Update the etag
+				obj.etag = generate_table_object_etag(obj)
+				ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(obj.save))
+
 				# Save that the user was active
 				user.update_column(:last_active, Time.now)
 
@@ -950,7 +974,7 @@ class AppsController < ApplicationController
 				end
 
 				result["properties"] = properties
-				result["etag"] = generate_table_object_etag(obj)
+				result["etag"] = obj.etag
 				render json: result, status: 200
 			else
 				# The object is not a file
@@ -998,6 +1022,10 @@ class AppsController < ApplicationController
 				# Reload the table object
 				obj = TableObject.find_by_id(obj.id)
 
+				# Update the etag
+				obj.etag = generate_table_object_etag(obj)
+				ValidationService.raise_validation_error(ValidationService.validate_unknown_validation_error(obj.save))
+
 				# Save that the user was active
 				user.update_column(:last_active, Time.now)
 
@@ -1018,7 +1046,7 @@ class AppsController < ApplicationController
 				end
 
 				result["properties"] = properties
-				result["etag"] = generate_table_object_etag(obj)
+				result["etag"] = obj.etag
 
 				render json: result, status: 200
 			end
@@ -1334,11 +1362,17 @@ class AppsController < ApplicationController
 				
 				if selected_table_objects
 					selected_table_objects.each do |table_object|
+						# Generate the etag if the table object has none
+						if table_object.etag.nil?
+							table_object.etag = generate_table_object_etag(table_object)
+							table_object.save
+						end
+
 						object = Hash.new
 						object["id"] = table_object.id
 						object["table_id"] = table_object.table_id
 						object["uuid"] = table_object.uuid
-						object["etag"] = generate_table_object_etag(table_object)
+						object["etag"] = table_object.etag
 						
 						array.push(object)
 					end
@@ -1423,11 +1457,17 @@ class AppsController < ApplicationController
 
 				if selected_table_objects
 					selected_table_objects.each do |table_object|
+						# Generate the etag if the table object has none
+						if table_object.etag.nil?
+							table_object.etag = generate_table_object_etag(table_object)
+							table_object.save
+						end
+
 						object = Hash.new
 						object["id"] = table_object.id
 						object["table_id"] = table_object.table_id
 						object["uuid"] = table_object.uuid
-						object["etag"] = generate_table_object_etag(table_object)
+						object["etag"] = table_object.etag
 						
 						array.push(object)
 					end
@@ -1480,18 +1520,17 @@ class AppsController < ApplicationController
 			TableObjectUserAccess.where(user_id: user_id).each { |access| all_table_objects.push(access.table_object) if access.table_alias == table.id }
 
 			all_table_objects.each do |obj|
+				# Generate the etag if the table object has none
+				if obj.etag.nil?
+					obj.etag = generate_table_object_etag(obj)
+					obj.save
+				end
+
 				object = Hash.new
 				object["id"] = obj.id
 				object["table_id"] = table_object.table_id
 				object["uuid"] = obj.uuid
-				object["etag"] = generate_table_object_etag(obj)
-
-				properties = Hash.new
-				obj.properties.each do |prop|
-					properties[prop.name] = prop.value
-				end
-
-				object["properties"] = properties
+				object["etag"] = obj.etag
 
 				array.push(object)
 			end
